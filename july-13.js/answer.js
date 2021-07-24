322446834d621e0c2608d6e48ba21ddd517d415d
Cat <morancathy@gmail.com>
2021-07-24 17:40:36 -0500

    added coding challenge from july13

---
+++ july-13.js/answer.js
@@ -0,0 +1,54 @@
+
+/*  Anser the Following:
+1. boolean.   const light = false.
+
+2. string. const userEmail = dieselcathy@gmail.com
+
+3. class.
+  class SpaceShipMoran {
+    constructor(hull, laserblaster, tractorbeam, warpdrive) {
+      this.hull = hull;
+      this.laserblaster = laserblaster;
+      this.tractorbeam = tractorbeam;
+      this.warpdrive = warpdrive;
+    }
+  }
+
+4. array.
+  let listOfStudents = ['Cathy', 'John', 'Jamie', 'Lillia']
+
+5. array (of objects).
+  const students = [
+    {
+    name: 'Cathy',
+    location: 'Chi-Town!'
+    },
+    {
+    name: 'Jason',
+    location: 'Virginia'
+    },
+    {
+    name: 'Sarah',
+    location: 'Washington State'
+    }
+  ]
+6. array.
+    const students = [
+    {
+    name: 'Cathy',
+    location: 'Chi-Town!',
+    favShow: 'Tom and Jerry'
+    },
+    {
+    name: 'Jason',
+    location: 'Virginia',
+    favShow: 'Small Wonder'
+    },
+    {
+    name: 'Sarah',
+    location: 'Washington State',
+    favShow: 'Three's Company'
+    }
+  ]
+
+*/
