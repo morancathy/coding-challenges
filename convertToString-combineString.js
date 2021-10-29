8629d1d445f2d44736fc89b4dc5a54d3d1faae72
Cat <morancathy@gmail.com>
2021-10-28 20:01:32 -0500

    covert to string

---
+++ convertToString-combineString.js
@@ -0,0 +1,8 @@
+// Convert to String
+const convertToString = (num1, num2, num3) => {
+  let addNums = num1 + num2 + num3;
+  let addNumsString = addNums.toString();
+  return addNumsString;
+ };
+
+console.log("Convert To String: ", convertToString(1, 6, 2));
