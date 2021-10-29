ad7567bd3236df737baf6878994972d90829a59c
Cat <morancathy@gmail.com>
2021-10-28 19:36:02 -0500

    fizzbuzz

---
+++ fizzbuzz.js
@@ -0,0 +1,19 @@
+// Fizz Buzz Challenge
+console.log('FizzBuzz Challenge:')
+
+function fizzBuzz(num) {
+
+for (let i = 1; i < num + 1; i++) {
+    if (i % 3 === 0 && i % 5 === 0){
+      console.log("FIZBUZZ!")
+    } else if(i % 3 === 0){
+      console.log('Fizz')
+    } else if(i % 5 === 0) {
+      console.log("Buzz")
+    } else {
+      console.log(i)
+    }
+  }
+}
+
+fizzBuzz(100);
