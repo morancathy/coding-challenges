7b75d4d9114a3c43396bdaa9a3f23b169e923015
Cat <morancathy@gmail.com>
2021-10-28 20:03:23 -0500

    real convert to String

--- convertToString-combineString.js
+++ convertToString-combineString.js
@@ -1,8 +1,19 @@
 // Convert to String
 const convertToString = (num1, num2, num3) => {
-  let addNums = num1 + num2 + num3;
-  let addNumsString = addNums.toString();
-  return addNumsString;
- };
 
-console.log("Convert To String: ", convertToString(1, 6, 2));
+  let numString = num1 + " " + num2 + " " + num3;
+  numString.toString();
+
+  console.log("Data type:", typeof(numString))
+  return numString.toString();
+ };
+console.log("Numbers turned to String:", convertToString(1, 6, 2));
+
+//########################################################################
+
+// Combine Strings into Sentence
+const makeSentence = (param1, param2, param3, param4) => {
+  console.log(`Combine strings: ${param1} really ${param2} ${param3} hot ${param4}... now!`)
+};
+
+makeSentence('I', 'want', 2, 'chimichangas');
