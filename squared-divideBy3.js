e89b3980b8ae05218f9c23313ed4ed763df738e6
Cat <morancathy@gmail.com>
2021-10-28 19:59:37 -0500

    divide by 3 nums

--- squared-divideBy3.js
+++ squared-divideBy3.js
@@ -4,3 +4,12 @@ const square = (num) => {
   console.log("Squared Number: ", squaredNumber)
 }
 square(5)
+//########################################################################
+
+// Divide 3 Numbers
+const divideThreeNums = (num1, num2, num3) => {
+  let numA = num2/num1;
+  let numB = num3/numA;
+  console.log("Divide Three Numbers: ", numB)
+}
+divideThreeNums(10, 5, 2)
