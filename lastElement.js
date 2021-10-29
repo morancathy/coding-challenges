7dd49f3415efc27bd8a75b0fbb77916590175b94
Cat <morancathy@gmail.com>
2021-10-28 20:04:24 -0500

    last element in array

---
+++ lastElement.js
@@ -0,0 +1,7 @@
+// Get Last Element
+const getLastElement = (arr) => {
+  let lastElement = arr[arr.length -1]
+  console.log("Last Element in Array: ", lastElement)
+};
+
+getLastElement([1, 2, 3, 4, 5, 6]);
