5eb587b8583841663ce61627572bb887a16ffeef
Cat <morancathy@gmail.com>
2021-11-03 14:50:27 -0500

    min/max num in array/ min/max combo of 4 ints in array

--- hackerrank.js
+++ hackerrank.js
@@ -22,3 +22,32 @@ function plusMinus(arr) {
     console.log(zero.toFixed(6))
 }
 plusMinus([-1, 2, 0])
+///////////////////////////////////////////////////////////////////////////////
+// Finding Max and Min in an array
+//Given five positive integers, find the minimum and maximum values that
+// can be calculated by summing exactly four of the five integers.
+function miniMaxSum(arr) {
+  let min = Math.min(...array)
+  let max = Math.max(...array)
+  let minTot = 0;
+  let maxTot = 0;
+
+// find min number in Array, add the rest to find total of largest nums
+  const minIndex = arr.indexOf(min)
+  arr.splice(minIndex, 1)
+  for(let i= 0; i < arr.length; i++) {
+    maxTot = maxTot + arr[i]
+  }
+  arr.push(min)
+
+// find max number in Array, add the rest to find total of smallest nmbs
+  const maxIndex = arr.indexOf(max)
+  arr.splice(maxIndex, 1)
+  for(let i= 0; i < arr.length; i++) {
+    minTot = minTot + arr[i]
+  }
+
+  console.log(minTot, maxTot)
+}
+const array = [2, 1, 3, 4, 5]
+miniMaxSum(array)
