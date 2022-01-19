2f5f025361f6eed9c6ad29bb3ad79e99cd1d4b3f
Cat <morancathy@gmail.com>
2022-01-19 15:28:32 -0600

    frequency counter pattern problem

---
+++ frequencyCounterPatterns.js
@@ -0,0 +1,35 @@
+/*
+Write a function which accepts two arrays. The function should return true if every value in the array has it's corresponding value squared in the second array. The frequency of values must be the same.
+*/
+
+function same(arr1, arr2){
+    if(arr1.length !== arr2.length) { return false}
+
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+
+    for(let val of arr1){
+        console.log("val", val)
+       frequencyCounter1[val] = (frequencyCounter1[val] || 0) + 1;
+        console.log(frequencyCounter1)
+        console.log(frequencyCounter1[val])
+    }
+
+    for (let val of arr2) {
+        frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+    }
+
+    for (let key in frequencyCounter1) {
+        if(!(key ** 2 in frequencyCounter2)) {
+            return false
+        }
+        if(frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+            return false
+        }
+    }
+    return true
+}
+
+console.log(same([1,2,3], [4,1,9])) // true
+console.log(same([1,2,3], [1,9])) // false
+console.log(same([1,2,1], [4,4,1, 4])) // false (must be same frequency)
